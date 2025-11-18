09586d07f18f2a0babfb8a6308429f6e8ca6eebf
armandwipangestu <armandwi.pangestu7@gmail.com>
2025-11-18 08:38:42 +0700

    fix(typo): change longtitude to longitude

--- models/map.go
+++ models/map.go
@@ -12,7 +12,7 @@ type Map struct {
 	Description		string			`json:"description" gorm:"type:text"`
 	Address			string			`json:"address" gorm:"type:text"`
 	Latitude		string			`json:"latitude" gorm:"type:double"`
-	Longtitude		string			`json:"longtitude" gorm:"type:double"`
+	Longitude		string			`json:"longitude" gorm:"type:double"`
 	Geometry		string			`json:"geometry" gorm:"type:json"`
 	Status			string			`json:"status" gorm:"type:enum('active', 'inactive');default:'active'"`
 	CategoryID		uint			`json:"category_id"`
