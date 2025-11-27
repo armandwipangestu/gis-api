6106691133c66214bac32e402a9e0376d4eb90e7
armandwipangestu <armandwi.pangestu7@gmail.com>
2025-11-27 17:03:43 +0700

    feat(api): add RESTful API category UPDATE by id

--- controllers/admin/category_controller.go
+++ controllers/admin/category_controller.go
@@ -6,6 +6,8 @@ import (
 	"armandwipangestu/gis-api/models"
 	"armandwipangestu/gis-api/structs"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -148,6 +150,102 @@ func FindCategoryById(c *gin.Context) {
 	c.JSON(http.StatusOK, structs.SuccessResponse{
 		Success: true,
 		Message: "Category found",
+		Data:	 category,
+	})
+}
+
+// Update data category
+func UpdateCategory(c *gin.Context) {
+	// Get parameter id
+	id := c.Param("id")
+
+	// Initialize category
+	var category models.Category
+
+	// Check if category with that id is exist or not
+	if err := database.DB.First(&category, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "Category not found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+
+		return
+	}
+
+	// Struct category update request
+	var req structs.CategoryUpdateRequest
+
+	// Input validation
+	if err := c.ShouldBind(&req); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, structs.ErrorResponse{
+			Success: false,
+			Message: "Validation Errors",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+
+		return
+	}
+
+	// Save path old image (if exist)
+	oldImagePath := ""
+	if category.Image != "" {
+		oldImagePath = filepath.Join("public", "uploads", "categories", category.Image)
+	}
+
+	// If user upload new image
+	file, err := c.FormFile("image")
+	if err == nil {
+		// Upload new image
+		uploadResult := helpers.UploadFile(c, helpers.UploadConfig{
+			File:			file,
+			AllowedTypes: 	[]string{".jpg", ".jpeg", ".png", ".gif"},
+			MaxSize: 		10 << 20, // Maximum file upload is 10MB
+			DestinationDir: "public/uploads/categories",
+		})
+
+		// If upload is fail, return error
+		if uploadResult.Response != nil {
+			c.JSON(http.StatusBadRequest, uploadResult.Response)
+			
+			return
+		}
+
+		// Set new filename
+		category.Image = uploadResult.FileName
+	}
+
+	// Update data
+	category.Name = req.Name
+	category.Slug = helpers.Slugify(req.Name)
+	category.Color = req.Color
+	category.Description = req.Description
+
+	// Save changes to database
+	if err := database.DB.Save(&category).Error; err != nil {
+		// If save fail and has new file, remove new file, to make not orphan
+		if file != nil && category.Image != "" {
+			newImagePath := filepath.Join("public", "uploads", "categories", category.Image)
+			os.Remove(newImagePath)
+		}
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "failed to update category",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+
+		return
+	}
+
+	// If has new file and old file still exist, remove old image
+	if file != nil && oldImagePath != "" {
+		_ = os.Remove(oldImagePath)
+	}
+
+	// Send success response
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "Category updated successfully",
 		Data:	 category,
 	})
 }
