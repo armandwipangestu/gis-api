67cce8451426fb47c09a571fab91df423bc7d0b3
armandwipangestu <armandwi.pangestu7@gmail.com>
2025-11-27 23:56:56 +0700

    feat(api): add CORS configuration

--- routes/routes.go
+++ routes/routes.go
@@ -6,12 +6,21 @@ import (
 	publicController "armandwipangestu/gis-api/controllers/public"
 	"armandwipangestu/gis-api/middlewares"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	// Initialize gin
 	router := gin.Default()
+
+	// Setup CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: 	[]string{"*"},
+		AllowMethods: 	[]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders: 	[]string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: 	[]string{"Content-Length"},
+	}))
 
 	// Auth routes (no auth required)
 	auth := router.Group("/api")
